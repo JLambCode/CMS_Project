ece9bc6e9526224e302c9535bda73884c6d980f7
Jeremy <66347648+JLambCode@users.noreply.github.com>
2022-06-02 11:58:54 -0400

    Create admin_nav.php

---
+++ cms/admin/includes/admin_nav.php
@@ -0,0 +1,66 @@
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">CMS Admin</a>
+            </div>
+
+            <!-- Top Menu Items -->
+            <ul class="nav navbar-right top-nav">
+                <li><a href="../index.php">Home Site</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo $_SESSION['user_first_name'];?> <?php echo $_SESSION['user_last_name'];?><b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="../includes/logout.php"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+
+            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <li>
+                        <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="./posts.php"><i class="fa fa-solid fa-align-left"></i> View All Posts</a>
+                    </li>
+                    <li>
+                        <a href="./categories.php"><i class="fa fa-solid fa-book"></i> Categories</a>
+                    </li>
+                    
+                    <li>
+                        <a href="comments.php"><i class="fa fa-regular fa-comments"></i> Comments</a>
+                    </li>
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i>Users<i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="demo" class="collapse">
+                            <li>
+                                <a href="./users.php"> View All Users</a>
+                            </li>
+                            <li>
+                                <a href="./users.php?source=add_user"> Add User</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="profile.php"><i class="fa fa-solid fa-user"></i> Profile</a>
+                    </li>
+
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </nav>
