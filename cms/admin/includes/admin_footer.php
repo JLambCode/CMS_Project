5f69b22722c3a35434bbf9bfbc137c5f33076d68
Jeremy <66347648+JLambCode@users.noreply.github.com>
2022-06-02 11:58:52 -0400

    Create admin_footer.php

---
+++ cms/admin/includes/admin_footer.php
@@ -0,0 +1,15 @@
+        <!-- jQuery -->
+        <script src="./js/jquery.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="./js/bootstrap.min.js"></script>
+
+        <!--Summer Note wysiwyg Javascipt -->
+        <script src="./js/summernote.js"></script>
+        <!-- <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script> -->
+
+        <script src="./js/scripts.js"></script>
+
+    </body>
+
+</html>
