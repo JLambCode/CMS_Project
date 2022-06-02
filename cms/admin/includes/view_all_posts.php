59c0f72f2ca14f7724834a3a4b4edbdd9a814580
Jeremy <66347648+JLambCode@users.noreply.github.com>
2022-06-02 11:59:03 -0400

    Create view_all_posts.php

---
+++ cms/admin/includes/view_all_posts.php
@@ -0,0 +1,72 @@
+<?php
+    if(isset($_POST['checkBoxArray'])){
+        foreach($_POST['checkBoxArray'] as $post_value_id){
+            $bulk_options = $_POST['bulk_options'];
+
+            switch($bulk_options){
+                case 'PUBLISHED':
+                    $query = "UPDATE posts SET post_status = '{$bulk_options}' WHERE post_id = {$post_value_id}";
+                    $update_to_published_status = mysqli_query($connection, $query);
+                    confirm_query($update_to_published_status);
+
+                    break;
+
+                case 'DRAFT':
+                    $query = "UPDATE posts SET post_status = '{$bulk_options}' WHERE post_id = {$post_value_id}";
+                    $update_to_draft_status = mysqli_query($connection, $query);
+                    confirm_query($update_to_draft_status);
+
+                    break;
+
+                case 'delete':
+                    $query = "DELETE FROM posts WHERE post_id = {$post_value_id}";
+                    $delete_selected_posts = mysqli_query($connection, $query);
+                    confirm_query($delete_selected_posts);
+
+                    break;
+            }
+        }
+    }
+?>
+
+<form action="" method="post">
+    <table class="table table-bordered table-hover">
+        <h4>Bulk Options</h4>
+        <div id="bulkOptionsContainer" class="col-xs-1" style="padding:">
+            <select class="form-control" name="bulk_options" id="">
+                <option value="PUBLISHED">Publish</option>
+                <option value="DRAFT">Draft</option>
+                <option value="delete">Delete</option>
+            </select>
+        </div>
+        <div class="col-xs-4">
+            <input type="submit" name="submit" class="btn btn-success" value="Apply">
+            <a class="btn btn-primary" href="./posts.php?source=add_post">Add New</a>
+        </div>
+        <thead>
+            <tr>
+                <th><input id="selectAllBoxes" type="checkbox"></th>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Date</th>
+                <th>Category</th>
+                <th>Status</th>
+                <th>Image</th>
+                <th>Tags</th>
+                <th>Comments</th>
+                <th>Edit Post</th>
+                <th>Delete Post</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+
+                    find_all_posts();
+
+                    delete_post();
+
+            ?>
+        </tbody>
+        
+    </table>
+</form>
