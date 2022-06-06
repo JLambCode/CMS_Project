a00ab10a05b11037e715a758a184fff46e48004c
Jeremy <66347648+JLambCode@users.noreply.github.com>
2022-06-05 22:48:37 -0400

    Update edit_user.php

--- cms/admin/includes/edit_user.php
+++ cms/admin/includes/edit_user.php
@@ -39,6 +39,17 @@
             }
         }
 
+        $query = "SELECT randSalt FROM users";
+        $select_randsalt_query = mysqli_query($connection, $query);
+        if(!$select_randsalt_query){
+            die("QUERY FAILED " . mysqli_error($connection));
+        }
+
+        $row = mysqli_fetch_assoc($select_randsalt_query);
+        $salt = $row['randSalt'];
+
+        $user_password = crypt($user_password, $salt);
+
         $query = "UPDATE users SET ";
         $query.= "user_name = '{$user_name}', ";
         $query.= "user_password = '{$user_password}', ";
