c20f0c879ff715a18093aea7aec6e69dde1a89be
Jeremy <66347648+JLambCode@users.noreply.github.com>
2022-06-05 22:40:34 -0400

    Update login.php

--- cms/includes/login.php
+++ cms/includes/login.php
@@ -25,6 +25,8 @@
             $db_user_role = $row['user_role'];
         }
 
+        $password = crypt($password, $db_user_password);
+
     }
 
     if($username === $db_user_name && $password === $db_user_password){
