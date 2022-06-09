5857ac7e6791788c21481725d911c67dfac5e18e
Jeremy <66347648+JLambCode@users.noreply.github.com>
2022-06-09 14:27:32 -0400

    Update index.php

--- cms/index.php
+++ cms/index.php
@@ -33,7 +33,7 @@
                         <a href="post.php?post_id=<?php echo $post_id; ?>"><?php echo $post_title ?></a>
                     </h1>
                     <p class="lead">
-                        by <a href="index.php"><?php echo $post_author ?></a>
+                        by <a href="author_posts.php?author=<?php echo $post_author ?>&post_id=<?php echo $post_id ?>"><?php echo $post_author ?></a>
                     </p>
                     <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
                     <hr>
